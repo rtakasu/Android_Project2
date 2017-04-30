5f97d300931f3efdee2f7ceebb5f666f78050ecb
itorras13 <itorras13@gmail.com>
2017-04-30 02:03:19 -0400

    added selection sort

--- app/src/main/java/com/example/rafaeltakasu/project2/MainActivity.java
+++ app/src/main/java/com/example/rafaeltakasu/project2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rafaeltakasu.project2;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AlertDialog;
@@ -21,15 +22,21 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 import java.util.TimerTask;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webview;
-    private EditText editText;
+    private EditText editBrowserText;
+    private EditText editSelectionSortText;
     private RelativeLayout tab1;
     private RelativeLayout tab2;
     private RelativeLayout tab3;
+    private TextView timerTextView;
+    private TextView selectionSortTextView;
 
     private static final String TAG = "Main";
     private ProgressDialog progressBar;
@@ -38,7 +45,25 @@ public class MainActivity extends AppCompatActivity {
     private Button min_brightness_button;
     private Button med_brightness_button;
     private Button max_brightness_button;
+    private Button selectionSortButton;
 
+    long startTime = 0;
+    //runs without a timer by reposting this handler at the end of the runnable
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+
+            timerTextView.setText(String.format("%d:%02d", minutes, seconds));
+
+            timerHandler.postDelayed(this, 500);
+        }
+    };
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -50,11 +75,13 @@ public class MainActivity extends AppCompatActivity {
                     tab1.setVisibility(View.VISIBLE);
                     tab2.setVisibility(View.INVISIBLE);
                     tab3.setVisibility(View.INVISIBLE);
+                    selectionSortTextView.setText("");
                     return true;
                 case R.id.navigation_brightness:
                     tab1.setVisibility(View.INVISIBLE);
                     tab2.setVisibility(View.VISIBLE);
                     tab3.setVisibility(View.INVISIBLE);
+                    selectionSortTextView.setText("");
                     return true;
                 case R.id.navigation_algorithms:
                     tab1.setVisibility(View.INVISIBLE);
@@ -77,7 +104,10 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         this.webview = (WebView)findViewById(R.id.webview);
-        this.editText = (EditText)findViewById(R.id.editText);
+        this.editBrowserText = (EditText)findViewById(R.id.editBrowserText);
+        this.editSelectionSortText = (EditText)findViewById(R.id.editSelectionSortText);
+        this.timerTextView = (TextView)findViewById(R.id.timerTextView);
+        this.selectionSortTextView = (TextView)findViewById(R.id.selectionSortTextView);
         this.tab1 = (RelativeLayout)findViewById(R.id.tab1);
         this.tab2 = (RelativeLayout)findViewById(R.id.tab2);
         this.tab3 = (RelativeLayout)findViewById(R.id.tab3);
@@ -105,12 +135,12 @@ public class MainActivity extends AppCompatActivity {
         });
         webview.loadUrl("https://www.google.com");
 
-        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        editBrowserText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 boolean handled = false;
                 if (actionId == EditorInfo.IME_ACTION_SEND) {
-                    webview.loadUrl(editText.getText().toString());
+                    webview.loadUrl(editBrowserText.getText().toString());
 
                     handled = true;
                 }
@@ -118,10 +148,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        selectionSortButton = (Button) findViewById(R.id.selectionSortButton);
 
         min_brightness_button = (Button) findViewById(R.id.min_brightness);
         med_brightness_button = (Button) findViewById(R.id.med_brightness);
         max_brightness_button = (Button) findViewById(R.id.max_brightness);
+
+        SelectionSortClickListener selectionClickListener = new SelectionSortClickListener();
+        selectionSortButton.setOnClickListener(selectionClickListener);
 
         // onClickListener for the brightness button
         MinBrightnessClickListener minOnClickListener = new MinBrightnessClickListener();
@@ -130,6 +164,46 @@ public class MainActivity extends AppCompatActivity {
         med_brightness_button.setOnClickListener(medOnClickListener);
         MaxBrightnessClickListener maxOnClickListener = new MaxBrightnessClickListener();
         max_brightness_button.setOnClickListener(maxOnClickListener);
+    }
+
+    private class SelectionSortClickListener implements View.OnClickListener  {
+        @Override
+        public void onClick(View v)  {
+            int listSize = 0;
+            boolean complete = false;
+            try {
+                Random rand = new Random();
+                String size = editSelectionSortText.getText().toString();
+                listSize = Integer.parseInt(size);
+                if(listSize < 100000000) {
+                    int list[] = new int[listSize];
+                    for (int i = 0; i < list.length; i++) {
+                        list[i] = rand.nextInt(listSize);
+                    }
+                    int min;
+                    for (int i = 0; i < list.length; i++) {
+                        min = i;
+                        for (int j = i + 1; j < list.length; j++) {
+                            if (list[j] < list[min]) {
+                                min = j;
+
+                            }
+                        }
+                        if (min != i) {
+                            final int temp = list[i];
+                            list[i] = list[min];
+                            list[min] = temp;
+                        }
+                    }
+                    selectionSortTextView.setText("Done!");
+                } else {
+                    selectionSortTextView.setText(size + " is too large!");
+                }
+            } catch(NumberFormatException nfe) {
+                selectionSortTextView.setText("Could not parse " + nfe);
+                System.out.println("Could not parse " + nfe);
+            }
+        }
 
     }
 
@@ -137,6 +211,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
 
+            startTime = System.currentTimeMillis();
+            timerHandler.postDelayed(timerRunnable, 0);
             WindowManager.LayoutParams layout = getWindow().getAttributes();
             layout.screenBrightness = 0F;
             getWindow().setAttributes(layout);
@@ -149,6 +225,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
 
+            startTime = System.currentTimeMillis();
+            timerHandler.postDelayed(timerRunnable, 0);
             WindowManager.LayoutParams layout = getWindow().getAttributes();
             layout.screenBrightness = 0.5F;
             getWindow().setAttributes(layout);
@@ -161,6 +239,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
 
+            startTime = System.currentTimeMillis();
+            timerHandler.postDelayed(timerRunnable, 0);
             WindowManager.LayoutParams layout = getWindow().getAttributes();
             layout.screenBrightness = 1F;
             getWindow().setAttributes(layout);
